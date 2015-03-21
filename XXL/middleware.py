9730c1dfc5beae270bbff860c473a9b348235a56
patsykakaz <philippe@lesidecar.fr>
2015-03-22 00:13:19 +0100

    middleware done right this time

--- XXL/middleware.py
+++ XXL/middleware.py
@@ -1,12 +1,10 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.template import Template, RequestContext
-from mezzanine.utils.device import templates_for_device
 
 class PubMiddleware(object):
     def process_template_response(self, request, response):
-        xxx = "test"
-        response.context_data['xxx'] = xxx
+        pubTag = "on"
+        response.context_data['pubTag'] = pubTag
         print(response.context_data)
         return response
 
