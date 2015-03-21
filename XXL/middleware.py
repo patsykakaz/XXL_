a4e0ec84a59bbf900e74f6c689358c1e1628d0fc
patsykakaz <philippe@lesidecar.fr>
2015-03-21 23:44:28 +0100

    finally built my middleware

--- XXL/middleware.py
+++ XXL/middleware.py
@@ -1,10 +1,19 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.template import Context
+from django.template import Template, RequestContext
+from mezzanine.utils.device import templates_for_device
 
 class PubMiddleware(object):
     def process_template_response(self, request, response):
-        """ OBJECTIF : verifier la présence de pub et 
-        ajouter au context les éléments """
+        xxx = "test"
+        response.context_data['xxx'] = xxx
+        print(response.context_data)
         return response
 
+
+# from models import Book
+
+# class GetAllBooks(object):
+#     def process_request(self, request):
+#         books = Book.objects.all()
+#         request.session['app_books'] = books
